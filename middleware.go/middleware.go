4d127a0156208156c7748b5f78d9ba2ca74b3b80
Jose Valdés <>
2025-01-13 19:09:50 +0100

    Now we are checking that the user is registered in the db before allowing any query even if the token is valid

--- middleware.go/middleware.go
+++ middleware.go/middleware.go
@@ -2,11 +2,14 @@ package middleware
 
 import (
 	"context"
+	"filmatch/database"
+	"filmatch/model"
 	"net/http"
 	"strings"
 
 	"firebase.google.com/go/v4/auth"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func FirebaseAuthMiddleware(client *auth.Client) gin.HandlerFunc {
@@ -27,12 +30,40 @@ func FirebaseAuthMiddleware(client *auth.Client) gin.HandlerFunc {
 			return
 		}
 
-		idToken := tokenParts[1]
-
 		// Let's verify the token with Firebase
+		idToken := tokenParts[1]
 		token, err := client.VerifyIDToken(context.Background(), idToken)
 		if err != nil {
 			currentContext.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token", "details": err.Error()})
+			currentContext.Abort()
+			return
+		}
+
+		// Let's get the user email from the token
+		email, ok := token.Claims["email"].(string)
+		if !ok || email == "" {
+			currentContext.JSON(http.StatusBadRequest, gin.H{"error": "Email not found in token"})
+			currentContext.Abort()
+			return
+		}
+
+		// If the endpoint is "/user/login", let's omit the database query, the user may not exist yet
+		if currentContext.Request.URL.Path == "/user/login" {
+			currentContext.Set("uid", token.UID)
+			currentContext.Set("email", email)
+			currentContext.Next()
+			return
+		}
+
+		// If it's any other endpoint, let's verify if the user exists in the database
+		var user model.User
+		if err := database.DB.Where("email = ?", email).First(&user).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				currentContext.JSON(http.StatusUnauthorized, gin.H{"error": "User not registered in the system"})
+				currentContext.Abort()
+				return
+			}
+			currentContext.JSON(http.StatusInternalServerError, gin.H{"error": "Database query failed", "details": err.Error()})
 			currentContext.Abort()
 			return
 		}
